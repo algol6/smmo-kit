acf902bbf3674f02747b6d20be0408597300388f
Vice <57636552+algol6@users.noreply.github.com>
2026-02-02 10:26:54 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+if __name__ == '__main__':
+
+    venv_path = 'VENV_PATH'
+
+    second_script_path = r"SCRIPT_PATH"
+
+    command = [
+        os.path.join(venv_path, 'Scripts', 'python.exe'),  
+        second_script_path
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+            print(f"Error while running the second script: {e}")
