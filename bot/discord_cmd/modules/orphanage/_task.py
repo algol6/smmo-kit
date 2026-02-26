bdc632db63bca21f46ce8db42102dcb2f98f0896
Vice <57636552+algol6@users.noreply.github.com>
2026-02-26 10:25:26 +0100

    update orphanage message

--- bot/discord_cmd/modules/orphanage/_task.py
+++ bot/discord_cmd/modules/orphanage/_task.py
@@ -26,6 +26,7 @@ class OrphanageTask(commands.Cog):
         TEMPLATES:str = ["[{bar}] {percentage}%\nGold :coin::\n> {x:,}/{y:,}\nStatus :repeat::\n> {status}\n",
                          "Bonuses :top::\n> {bonuses}\nActived since :clock4::\n> {actived_time}\nExpiring in :clock6::\n> {expiring_time1} {expiring_time2}"]
         BAR_LENGHT:int=15
+        progression:bool = True
         for tier_data in orphanage_data:
             tier = OrphanageTask.parse_orphanage_key_tier(tier_data.tier.key)
             for notification_config in orphanage_db:
@@ -35,20 +36,25 @@ class OrphanageTask(commands.Cog):
                                 thumbnail="https://simple-mmo.com/img/icons/orphanage.png",
                                 color= 0xffd700)
                     for i in range(3):
-                        temp = int((orphanage_data[i].current_value / orphanage_data[i].target_value)*BAR_LENGHT) * ":small_orange_diamond:"
-                        temp_status = "Active" if orphanage_data[i].is_active else "Inactive"
-                        temp_status = "Expired" if orphanage_data[i].has_expired else temp_status
+                        if not progression:
+                            continue
+                        if not(orphanage_data[i].is_active or orphanage_data[i].has_expired):
+                            progression = False
+                        ntemp = int((orphanage_data[i].current_value / orphanage_data[i].target_value)*BAR_LENGHT)
+                        temp = ntemp * ":small_orange_diamond:"
+                        temp_status = "Active :white_check_mark:" if orphanage_data[i].is_active else "Inactive :x:"
+                        temp_status = "Expired :zzz:" if orphanage_data[i].has_expired else temp_status
                         emb.add_field(name=f"Tier {i+1}",
                                     value=TEMPLATES[0].format(
-                                        bar=f"{temp}{(BAR_LENGHT - len(temp)) * ":small_blue_diamond:"}",
+                                        bar=f"{temp}{(BAR_LENGHT - ntemp) * ":small_blue_diamond:"}",
                                         percentage=orphanage_data[i].percentage,
                                         x=orphanage_data[i].current_value,
                                         y=orphanage_data[i].target_value,status=temp_status) + (TEMPLATES[1].format(
-                                        bonuses="\n> ".join(orphanage_data[i].effects),
-                                        actived_time=f"<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp())}:R>" if orphanage_data[i].is_active else "Not yet",
-                                        expiring_time1=f"<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp() + 7200)}:R>" if orphanage_data[i].is_active else "Not yet",
-                                        expiring_time2=f"(<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp() + 7200)}>)" if orphanage_data[i].is_active else "+ 1"
-                                        ) if orphanage_data[i].is_active else ""),
+                                            bonuses="\n> ".join(orphanage_data[i].effects),
+                                            actived_time=f"<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp())}:R>" if orphanage_data[i].is_active else "Not yet",
+                                            expiring_time1=f"<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp() + 7200)}:R>" if orphanage_data[i].is_active else "Not yet",
+                                            expiring_time2=f"(<t:{int(datetime.fromisoformat(orphanage_data[i].goal_reached_at).timestamp() + 7200)}>)" if orphanage_data[i].is_active else "+ 1"
+                                            ) if orphanage_data[i].is_active else ""),
                                         inline=False)
                     emb.set_footer(text=f"Updated every 5 minutes")
                 if notification_config.channel_id not in channels_id:
@@ -70,7 +76,7 @@ class OrphanageTask(commands.Cog):
                                                         content=f"<@&{notification_config.role_id}>\nOrphanage **tier {tier}** is now active.",
                                                         delete_after=7200)
                 if not await Database.update_orphanage_active(notification_config.channel_id,notification_config.tier,int(tier_data.is_active)):
-                        logger.warning("Couldn't update orphanage status")
+                    logger.warning("Couldn't update orphanage status")
         
     @staticmethod
     def parse_orphanage_key_tier(tier_key: str):
