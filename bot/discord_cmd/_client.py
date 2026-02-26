1473d0dfbe18d636ea7d5efd9322e2912ffcf8e4
Vice <57636552+algol6@users.noreply.github.com>
2026-02-26 10:24:43 +0100

    update log

--- bot/discord_cmd/_client.py
+++ bot/discord_cmd/_client.py
@@ -41,7 +41,7 @@ async def on_application_command_error(ctx: ApplicationContext, error: DiscordEx
         except:
             guild = f"[{ctx.guild.name} #{ctx.channel}]"
 
-    logger.error("[/%s] from %s:\n%s",ctx.command.qualified_name,guild,error)
+    logger.error("COMMAND [/%s] from %s:\n%s",ctx.command.qualified_name,guild,error)
     if isinstance(error, errors.NotFound):
         logger.warning("Error 'discord.errors.NotFound'")
         return await helpers.send(ctx,content=f"Error with discord, Try again.")
