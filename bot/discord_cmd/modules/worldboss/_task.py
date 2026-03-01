6aa5b33f43432226a5e9d2e84e48a498b3680221
Vice <57636552+algol6@users.noreply.github.com>
2026-03-01 19:13:36 +0100

    cleaner code

--- bot/discord_cmd/modules/worldboss/_task.py
+++ bot/discord_cmd/modules/worldboss/_task.py
@@ -66,9 +66,11 @@ class WorldbossTasks(Cog):
         for d in data:
             if d.boss_id == wb.id:
                 continue
-            if wb.enable_time <= datetime.now().timestamp() + d.seconds_before:
-                await Database.update_wb_notification(d.channel_id, wb.id,d.seconds_before)
-                if d.god == 1 and not wb.god:
-                    continue
-                await helpers.get_channel_and_edit(self.client,d.channel_id,content=f"<@&{d.role_id}> world boss **{wb.name}** in about <t:{wb.enable_time}:R>", delete_after=(wb.enable_time - datetime.now().timestamp() + 60))
+            if wb.enable_time > datetime.now().timestamp() + d.seconds_before:
+                continue
+            await Database.update_wb_notification(d.channel_id, wb.id,d.seconds_before)
+            if d.god == 1 and not wb.god:
+                continue
+            print(f"sending wb ping messsage {wb.name}")
+            await helpers.get_channel_and_edit(self.client,d.channel_id,content=f"<@&{d.role_id}> world boss **{wb.name}** in about <t:{wb.enable_time}:R>", delete_after=(wb.enable_time - datetime.now().timestamp() + 60))
               
