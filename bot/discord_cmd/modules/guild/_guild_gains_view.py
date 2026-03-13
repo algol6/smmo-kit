ad5bf2335e3fb897e42f5560418441e2a9d17017
Vice <57636552+algol6@users.noreply.github.com>
2026-03-13 18:32:48 +0100

    updated old code

--- bot/discord_cmd/modules/guild/_guild_gains_view.py
+++ bot/discord_cmd/modules/guild/_guild_gains_view.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 
 class GuildGainsView(discord.ui.View):
     def __init__(self):
+        self.TITLE = ("Celestial","Legendary","Epic","Elite","Rare")
         super().__init__(timeout=None)
     
     current_page: int = 0
@@ -27,54 +28,27 @@ class GuildGainsView(discord.ui.View):
         if self.current_page == 0:
             self.prev_button.disabled = True
             self.prev_button.style = discord.ButtonStyle.gray
+
+            self.next_button.disabled = False
+            self.next_button.style = discord.ButtonStyle.primary
         else:
             self.prev_button.disabled = False
             self.prev_button.style = discord.ButtonStyle.primary
 
-        if self.current_page == 1:
             self.next_button.disabled = True
             self.next_button.style = discord.ButtonStyle.gray
-        else:
-            self.next_button.disabled = False
-            self.next_button.style = discord.ButtonStyle.primary
 
     async def create_embed(self):
         msg: str = ""
-        title: list = ["Celestial","Legendary","Epic","Elite","Rare"]
+        
         emb = helpers.Embed(title=f"{self.timeframe} guild gains",
                             description=f"**Stats**: from <t:{int(self.date.timestamp())}> to <t:{int(self.to_date.timestamp() + 86400)}>\n"
                                         f"**Last update**: <t:{self.last_update}:R>\n"
                                         f"**Season**: {self.season.id}\n"
                                         f"**Season Ending**: <t:{int(datetime.fromisoformat(self.season.ends_at[:-1]).timestamp())}>")
-        if self.current_page==0:
-            for count in range(5):
-                for i,g in enumerate(self.season_lb[count*5:5+(count*5)]):
-                        s = await Database.select_guild_stats(g.guild["id"], self.date.year,self.date.month,self.date.day,self.season.id)
-                        if self.timeframe == "Yesterday":
-                            if s is None:
-                                msg += "No data"
-                                continue
-                            temp_date = helpers.get_current_date_game()
-                            g_stats = await Database.select_guild_stats(g.guild["id"], temp_date.year,temp_date.month,temp_date.day,self.season.id)
-                            if g_stats is not None:
-                                g.experience = g_stats.experience
-                                g.position = s.position
-                        
-                        msg = f"{msg}{"\n" if i != 0 else ""}#{g.position} **[{g.guild['name']}](https://simple-mmo.com/guilds/view/{g.guild['id']})**: {g.experience:,}xp (+*{abs((s.experience if s is not None else 0)-g.experience):,}*)"
-                        if s is not None:
-                            if g.position == s.position:
-                                pass
-                            elif g.position<s.position:
-                                msg = f"{msg} :arrow_up: +{s.position-g.position}"
-                            else:
-                                msg = f"{msg} :arrow_down: {s.position-g.position}"
-                emb.add_field(name=title[count],
-                            value=msg,
-                            inline=False)
-                msg = ""
-        else:
-            for count in range(5):
-                for i,g in enumerate(self.season_lb[25+count*5:30+(count*5)]):
+        
+        for count in range(5):
+            for i,g in enumerate(self.season_lb[(25*self.current_page)+count*5:(25*self.current_page)+5+(count*5)]):
                     s = await Database.select_guild_stats(g.guild["id"], self.date.year,self.date.month,self.date.day,self.season.id)
                     if self.timeframe == "Yesterday":
                         if s is None:
@@ -94,21 +68,22 @@ class GuildGainsView(discord.ui.View):
                             msg = f"{msg} :arrow_up: +{s.position-g.position}"
                         else:
                             msg = f"{msg} :arrow_down: {s.position-g.position}"
-                emb.add_field(name="",
-                            value=msg,
-                            inline=False)
-                msg = ""
+            emb.add_field(name=self.TITLE[count] if self.current_page==0 else "",
+                        value=msg,
+                        inline=False)
+            msg = ""
+        
         emb.set_footer(text="Name Guild: Total GXP (+Daily GXP gained)")
         return emb
             
-    @discord.ui.button(label="<", style=discord.ButtonStyle.primary)
+    @discord.ui.button(label="<", style=discord.ButtonStyle.primary,custom_id="prev-gain-btn")
     async def prev_button(self, button:discord.ui.Button, interaction:discord.Interaction):
         await interaction.response.defer()
         self.current_page = 0
         await self.update_message()
 
 
-    @discord.ui.button(label=">", style=discord.ButtonStyle.primary)
+    @discord.ui.button(label=">", style=discord.ButtonStyle.primary,custom_id="next-gain-btn")
     async def next_button(self, button:discord.ui.Button, interaction:discord.Interaction):
         await interaction.response.defer()
         self.current_page = 1
