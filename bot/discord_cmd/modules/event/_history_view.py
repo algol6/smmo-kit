355580b26c00134fb43f6b0026f4c8cbaf78435f
Vice <57636552+algol6@users.noreply.github.com>
2026-03-18 21:08:11 +0100

    Create _history_view.py

---
+++ bot/discord_cmd/modules/event/_history_view.py
@@ -0,0 +1,98 @@
+import discord
+from bot.discord_cmd.helpers import helpers 
+
+class HistoryView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    
+    current_page: int = 1
+    sep: int = 10
+
+    async def send(self, ctx: discord.ApplicationContext):
+        self.ctx = ctx
+        self.message = await ctx.followup.send(view=self)
+        await self.update_message(self.event_list[:self.sep])
+        
+
+    async def update_message(self, data):
+        await self.update_buttons()
+        try:
+            await self.message.edit(embed=await self.create_embed(data), view=self)
+        except discord.errors.Forbidden:
+            await self.ctx.followup.send(content="Bot doesn't have the perms to see this channel, so the buttons of the message doesn't work.")
+
+
+    async def update_buttons(self):
+        if self.current_page == 1:
+            self.prev_button.disabled = True
+            self.prev_button.style = discord.ButtonStyle.gray
+            self.first_button.disabled = True
+            self.first_button.style = discord.ButtonStyle.gray
+        else:
+            self.prev_button.disabled = False
+            self.prev_button.style = discord.ButtonStyle.primary
+            self.first_button.disabled = False
+            self.first_button.style = discord.ButtonStyle.green
+
+        if self.current_page == int(len(self.event_list)/self.sep) + 1:
+            self.next_button.disabled = True
+            self.next_button.style = discord.ButtonStyle.gray
+            self.last_button.disabled = True
+            self.last_button.style = discord.ButtonStyle.gray
+        else:
+            self.next_button.disabled = False
+            self.next_button.style = discord.ButtonStyle.primary
+            self.last_button.disabled = False
+            self.last_button.style = discord.ButtonStyle.green
+
+
+    def get_current_page_data(self):
+        until_item = self.current_page * self.sep
+        from_item = until_item - self.sep
+        if self.current_page == 1:
+            from_item = 0
+            until_item = self.sep
+        if self.current_page == int((len(self.event_list)) / self.sep) + 1:
+            from_item = self.current_page * self.sep - self.sep
+            until_item = len(self.event_list)
+        return self.event_teams[from_item:until_item]
+    
+
+    async def create_embed(self, data):
+        emb = helpers.Embed(title=f"Event History:",
+                            description=f"Last update: <t:{self.last_update}:R>")
+        for v in data:
+            emb.add_field(name="",
+                          value=f"[{v.id}] - {v.name}",
+                          inline=False)
+        #emb.add_field(name="", value="\n".join([f"## **#{i}** - Team: {v[0]}\n   [{v["name"]}](https://simple-mmo.com/user/view/{v["id"]}): {format(v["stats"],",d")}" for v,i in zip(data,range(from_item + 1, until_item + 1))]))
+
+        emb.set_footer(text=f"Page {self.current_page}/{(len(self.event_list)) // self.sep + 1}")
+        return emb
+
+    @discord.ui.button(label="|<", style=discord.ButtonStyle.green)
+    async def first_button(self, button:discord.ui.Button, interaction:discord.Interaction):
+        await interaction.response.defer()
+        self.current_page = 1
+        await self.update_message(self.get_current_page_data())
+            
+
+    @discord.ui.button(label="<", style=discord.ButtonStyle.primary)
+    async def prev_button(self, button:discord.ui.Button, interaction:discord.Interaction):
+        await interaction.response.defer()
+        self.current_page -= 1
+        await self.update_message(self.get_current_page_data())
+
+
+    @discord.ui.button(label=">", style=discord.ButtonStyle.primary)
+    async def next_button(self, button:discord.ui.Button, interaction:discord.Interaction):
+        await interaction.response.defer()
+        self.current_page += 1
+        await self.update_message(self.get_current_page_data())
+
+
+    @discord.ui.button(label=">|", style=discord.ButtonStyle.green)
+    async def last_button(self, button:discord.ui.Button, interaction:discord.Interaction):
+        await interaction.response.defer()
+        self.current_page = int((len(self.event_list)) / self.sep) + 1
+        await self.update_message(self.get_current_page_data())
