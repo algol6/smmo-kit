c7fc4cfaca311692400339d64289ca5e295b59c0
algol6 <vincenzoassenza360@gmail.com>
2026-05-27 10:17:59 +0200

    fix and optimization

--- bot/discord_cmd/modules/worldboss/_task.py
+++ bot/discord_cmd/modules/worldboss/_task.py
@@ -17,6 +17,8 @@ class WorldbossTasks(Cog):
         self.check_worldboss.start()
         self.update_worldboss.start()
 
+        self.wb_cache = None
+
     def cog_unload(self):
         self.check_worldboss.cancel()
         self.update_worldboss.cancel()
@@ -32,16 +34,19 @@ class WorldbossTasks(Cog):
             if not await Database.insert_worldboss(wb.id,wb.name,wb.avatar,wb.level,int(wb.god),wb.strength,wb.defence,wb.dexterity,wb.current_hp,wb.max_hp,wb.enable_time):
                 logger.warning("Error while saving worldboss:%s",wb)
             
-    @loop(seconds=1)
+    
+    @loop(minutes=1)
     async def check_worldboss(self):
-        is_empty,worldboss_data = helpers.gen_is_empty(await Database.select_worldboss(int(datetime.now().timestamp()-10)))
-        if is_empty:
-            return
-        wb = next(worldboss_data)
-        await self.notify_wb(wb)
-        await self.update_message(wb)
+        if self.wb_cache is None or self.wb_cache.enable_time < datetime.now().timestamp():
+            temp = tuple(await Database.select_worldboss(int(datetime.now().timestamp()-65)))
+            if len(temp) == 0:
+                return
+            self.wb_cache = temp[0]
+        
+        await self.notify_wb(self.wb_cache)
+        await self.update_message(self.wb_cache)
 
-
+    
     async def update_message(self, wb_data:WorldBoss):
         data = await Database.select_wb_message()
         emb = helpers.Embed(title=wb_data.name,url="https://simple-mmo.com/worldboss/all",thumbnail=f"https://simple-mmo.com/img/sprites/{wb_data.avatar}.png")
@@ -71,5 +76,5 @@ class WorldbossTasks(Cog):
             await Database.update_wb_notification(d.channel_id, wb.id,d.seconds_before)
             if d.god == 1 and not wb.god:
                 continue
-            await helpers.get_channel_and_edit(self.client,d.channel_id,content=f"<@&{d.role_id}> world boss **{wb.name}** in about <t:{wb.enable_time}:R>", delete_after=(wb.enable_time - datetime.now().timestamp() + 60))
+            await helpers.get_channel_and_edit(self.client,d.channel_id,content=f"<@&{d.role_id}> world boss **{wb.name}** in about <t:{wb.enable_time}:R> ({d.seconds_before/60:,}m before)", delete_after=(wb.enable_time - datetime.now().timestamp() + 60))
               
